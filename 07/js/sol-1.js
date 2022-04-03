1e2acec48285c7703e4a196dbe8777c40a19a269
dyddlfsla <7942dyddlfsl@naver.com>
2022-04-04 01:01:19 +0900

    Mon Apr 4 : summary08-2

--- 07/js/sol-1.js
+++ 07/js/sol-1.js
@@ -1,17 +1,20 @@
 		var numbers = [2, 4, 6, 8, 10];
 		showArray(numbers);
-
-		var sum = 0;
-		for(var i=0; i<numbers.length; i++) {
-			sum += numbers[i];
-		}
-		numbers.push(sum);
+		pushLastSum(numbers);
 		showArray(numbers);
 
-		function showArray(arr) {
+		function pushLastSum(array) {
+			var sum = 0;
+			for (var i = 0; i < numbers.length; i++) {
+				sum += numbers[i];
+			}
+			numbers.push(sum);
+		}
+
+		function showArray(array) {
 			var str = "<table><tr>";
-			for (var i=0; i<arr.length; i++) {
-				str += "<td>" + arr[i] + "</td>";
+			for (var i=0; i<array.length; i++) {
+				str += "<td>" + array[i] + "</td>";
 			}
 			str += "</tr></table>";
 			document.write(str);
