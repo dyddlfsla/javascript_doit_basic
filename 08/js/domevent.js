e846564f533aa85b550a8b4c8e7620ac79ebea96
dyddlfsla <7942dyddlfsl@naver.com>
2022-04-08 02:05:07 +0900

    Fri Apr 8 : summary08-4

--- 08/js/domevent.js
+++ 08/js/domevent.js
@@ -1,4 +1,6 @@
 var pic = document.querySelector('#pic');
+pic.addEventListener("mouseover", changePic, false);
+pic.addEventListener("mouseout", originPic, false);
     
 function changePic() {			
   pic.src = "images/boy.png";
